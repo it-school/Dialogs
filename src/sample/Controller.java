5cbac31c8ec1490b848571117fce6815dc8fd6e0
Viktor Speranskyy <vencedor@ukr.net>
2018-12-07 14:34:26 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,134 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Controller {
+@FXML
+Label dateLabel;
+
+    public void Click(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog with Custom Actions");
+        alert.setHeaderText("Look, a Confirmation Dialog with Custom Actions");
+        alert.setContentText("Choose your option.");
+
+        ButtonType buttonTypeOne = new ButtonType("One");
+        ButtonType buttonTypeTwo = new ButtonType("Two");
+        ButtonType buttonTypeThree = new ButtonType("Three");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo, buttonTypeThree, buttonTypeCancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeOne) {
+            System.out.println("1");
+            // ... user chose "One"
+        } else if (result.get() == buttonTypeTwo) {
+            System.out.println("2");
+            // ... user chose "Two"
+        } else if (result.get() == buttonTypeThree) {
+            System.out.println("3");
+            // ... user chose "Three"
+        } else {
+            System.out.println("0");
+            // ... user chose CANCEL or closed the dialog
+        }
+    }
+
+    public void Click2(ActionEvent actionEvent) {
+        // Create the custom dialog.
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Login Dialog");
+        dialog.setHeaderText("Look, a Custom Login Dialog");
+
+// Set the icon (must be included in the project).
+//        dialog.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
+
+// Set the button types.
+        ButtonType loginButtonType = new ButtonType("Login", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+// Create the username and password labels and fields.
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField username = new TextField();
+        username.setPromptText("Username");
+        PasswordField password = new PasswordField();
+        password.setPromptText("Password");
+
+        grid.add(new Label("Username:"), 0, 0);
+        grid.add(username, 1, 0);
+        grid.add(new Label("Password:"), 0, 1);
+        grid.add(password, 1, 1);
+
+// Enable/Disable login button depending on whether a username was entered.
+        Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+        loginButton.setDisable(true);
+
+// Do some validation (using the Java 8 lambda syntax).
+        username.textProperty().addListener((observable, oldValue, newValue) -> {
+            loginButton.setDisable(newValue.trim().isEmpty());
+        });
+
+        dialog.getDialogPane().setContent(grid);
+
+// Request focus on the username field by default.
+        Platform.runLater(() -> username.requestFocus());
+
+// Convert the result to a username-password-pair when the login button is clicked.
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == loginButtonType) {
+                return new Pair<>(username.getText(), password.getText());
+            }
+            return null;
+        });
+
+
+        Optional<Pair<String, String>> result = null;
+        AtomicBoolean passed = new AtomicBoolean(false);
+        do {
+            result = dialog.showAndWait();
+            result.ifPresent(usernamePassword ->
+            {
+                System.out.println("Username=" + usernamePassword.getKey() + ", Password=" + usernamePassword.getValue());
+                if (usernamePassword.getKey().equals("qqq")) {
+                    System.out.println("OK");
+                    passed.set(true);
+                } else {
+                    final Tooltip tooltip = new Tooltip();
+                    tooltip.setText("\nYour login is wrong\n");
+                    username.setTooltip(tooltip);
+
+                    Platform.runLater(() -> username.requestFocus());
+                    username.setPromptText("Wrong LOGIN");
+                    System.out.println("Wrong login");
+                }
+            });
+
+
+        } while (passed.get() != true);
+
+
+    }
+
+    public void dateGet(ActionEvent actionEvent) {
+        Date date = new Date(1500000000000L);
+        dateLabel.setText(date.toGMTString());
+
+    }
+}
